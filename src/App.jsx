c36a5f7182b8d0a6df1f0cd4a418d6809dd25834
Rutesh98 <ruteshwin@gmail.com>
2024-02-22 13:04:52 +0530

    My TicTacToe game

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { useRef } from 'react'
+import React from 'react'
+import TicTacToe from './Components/TicTacToe/TicTacToe'
+const App = () => {
+return(
+  <div> 
+  <TicTacToe/>
+  </div>
+) 
+}
+
+export default App
